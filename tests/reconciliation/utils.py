11353e4012f164b56fe34264308053e2f53953f6
balamurugan-aicp <balamurugan@aicloudpods.com>
2025-07-23 19:23:48 +0100

    Testing Structure in scalability repo with the folders for unit, integration and reconciliation tests

--- tests/reconciliation/utils.py
+++ tests/reconciliation/utils.py
@@ -3,8 +3,15 @@ from pyspark.sql import DataFrame
 def get_record_count(df: DataFrame) -> int:
     return df.count()
 
-def load_table(spark, path: str) -> DataFrame:
+def load_parquet_table(spark, path: str) -> DataFrame:
     """
     Load a table or dataset from the given path.
     """
     return spark.read.parquet(path)
+
+
+def load_csv_table(spark, path: str) -> DataFrame:
+    """
+    Load a table or dataset from the given path.
+    """
+    return spark.read.csv(path)
