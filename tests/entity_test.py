bbb8c219e8355f301a43b6b98864508372f34a25
lakshmi <LakshmiVenkataRatnaKumari.Kovvuri@cognizant.com>
2025-07-28 16:30:02 +0100

    Added code for entity table for #955 	modified:   tests/entity_test.py 	new file:   tests/organisation.csv

--- tests/entity_test.py
+++ tests/entity_test.py
@@ -9,17 +9,52 @@ spark = SparkSession.builder.appName("PivotDataset").getOrCreate()
 
 # Load the CSV file into a DataFrame
 df = spark.read.option("header", True).csv("/home/lakshmi/entity_testing/entity_sample_data.csv")
+dataset_name_arg='transport-access-node'
+# Pivot the transport-access-node based on 'field' and 'value', grouped by 'entry-number'
+pivot_df = df.groupBy("entry-number","entity").pivot("field").agg(first("value"))
+pivot_df = pivot_df.drop("entry-number")
+pivot_df=pivot_df.withColumn("dataset", lit(dataset_name_arg)).withColumn("typology", lit("geography"))
 
-# Pivot the dataset based on 'field' and 'value', grouped by 'entry-number'
-pivot_df = df.groupBy("entry_number","entity").pivot("field").agg(first("value"))
-pivot_df = pivot_df.drop("entry_number", "organisation")
-pivot_df=pivot_df.withColumn("topology", lit("geography")).withColumn("organisation_entity", lit("600010"))
 # Create JSON column
-pivot_df = pivot_df.withColumn(
-    "json",
-    to_json(struct("naptan_code","bus_stop_type" ,"transport_access_node_type"))
-)
-pivot_df = pivot_df.drop("naptan_code","bus_stop_type","transport_access_node_type")
+#pivot_df = pivot_df.withColumn("json",to_json(struct("naptan-code","bus-stop-type" ,"transport-access-node-type")))
+#pivot_df.show()
+if 'geometry' not in pivot_df.columns:
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    pivot_df = pivot_df.withColumn('geometry', lit("").cast("string"))
+
+pivot_df = pivot_df.drop("geojson")
+
+
+organisation_df = spark.read.option("header", "true").csv("/home/lakshmi/entity_testing/organisation.csv")
+#organisation_df.show()
+
+pivot_df = pivot_df.join(organisation_df, pivot_df.organisation == organisation_df.organisation, "left") \
+    .select(pivot_df["*"], organisation_df["entity"].alias("organisation_entity"))
+#pivot_df["organisation"].show()
+pivot_df = pivot_df.drop("organisation")
+
+standard_column= ["dataset","end-date","entity","entry-date","geometry","json","name","organisation_entity","point","prefix","reference","start-date","typology"]
+pivot_df_columns=pivot_df.columns
+
+# Find difference
+difference_columns = list(set(pivot_df_columns) - set(standard_column))
+
+# Convert to JSON-like dictionary
+difference_json = {"difference_columns": difference_columns}
+
+
+
+# Collect the data for those columns
+difference_data = pivot_df.select(difference_columns).collect()
+
+# Convert to JSON-like dictionary
+difference_json = {
+    col: [row[col] for row in difference_data]
+    for col in difference_columns
+}
+print(difference_json)
+
+#pivot_df = pivot_df.withColumn("json",to_json(struct("naptan-code","bus-stop-type" ,"transport-access-node-type")))
 
 pivot_df.write.mode("overwrite").option("header", True) \
 .csv("/home/lakshmi/entity_testing/pivoted_entity_data.csv")
